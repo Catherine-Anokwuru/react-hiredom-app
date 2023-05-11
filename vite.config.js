cf6695a46e29b402a80f95657ac265b2a67230a1
CatherineAnokwuru <106893457+Catherine-Anokwuru@users.noreply.github.com>
2023-05-12 00:28:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//   base: "https://catherine-anokwuru.github.io/react-hiredom-app/",
+  base: "https://catherine-anokwuru.github.io/react-hiredom-app/",
   plugins: [react()],
 });
