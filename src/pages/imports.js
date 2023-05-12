83c1d6b9d10685afda4f0327b7db9c25ae84e111
Catherine-Anokwuru <catherineanokwuru@gmail.com>
2023-05-12 02:53:39 +0100

    Updates

--- src/pages/imports.js
+++
@@ -1,6 +0,0 @@
-import Dashboard from "./Dashboard";
-import Error from "./Error";
-import Landing from "./Landing";
-import Register from "./Register";
-
-export { Landing, Error, Dashboard, Register };
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://catherine-anokwuru.github.io/react-hiredom-app/",
-  plugins: [react()],
-});
